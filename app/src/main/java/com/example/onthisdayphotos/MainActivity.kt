b12e13e3b8c9bbf899828b1569b22ca0d3e6b86f
Juan Kerr <31308220+JuanKerr@users.noreply.github.com>
2026-03-12 13:46:32 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/onthisdayphotos/MainActivity.kt
+++ app/src/main/java/com/example/onthisdayphotos/MainActivity.kt
@@ -15,13 +15,11 @@ import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.stickyHeader
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
